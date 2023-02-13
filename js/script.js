76b9b335ba57e41b743d69eb5f2f22ab50504118
Florencia Cardinali <lorenciapaulacardinali@gmail.com>
2023-02-13 06:38:55 -0300

    carpeta js, formularios listos pero sin funcionar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// var botonNom = document.getElementsById("editar-nombre");
+
+// botonNom.addEventListener("mousedown", event => {
+//     let formNom = document.getElementsById("form-nombre");
+//     formNom.style.display =  "flex";
+//     let perfilNom = document.getElementsById("perfil-nombre");
+//     perfilNom.style.display= "none";
+//     botonNom.style.display= "none";
+// })
